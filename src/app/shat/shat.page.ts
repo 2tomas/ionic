89f6464ba42dc8e4d799413f47c70c967648bfbc
SR_I01505 <se.rocha@redlink.com.ar>
2024-02-26 01:51:09 -0300

    ionic

--- src/app/shat/shat.page.ts
+++ src/app/shat/shat.page.ts
@@ -3,6 +3,7 @@ import { Firestore, collection, collectionData, doc, getDoc, setDoc } from '@ang
 import { Observable } from 'rxjs';
 import { AuthServiceService } from '../service/auth-service.service';
 import { Router } from '@angular/router';
+import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
 
 @Component({
   selector: 'app-shat',
@@ -14,11 +15,23 @@ export class ShatPage {
   item: Observable<any[]>;
   public valor : string = "";
   usuario: any;
+  user: string | null = "";
 
-  constructor(public firestore: Firestore, public auth: AuthServiceService, private router: Router,) {
+  constructor(public firestore: Firestore, public auth: AuthServiceService, private router: Router,private androidPermissions: AndroidPermissions) {
+    this.user = localStorage.getItem('usuario');
     const itemCollection = collection(this.firestore, 'Mensaje');
     this.item = collectionData(itemCollection);
 
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+      result => console.log('Has permission?',result.hasPermission),
+      err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
+    );
+    
+    this.androidPermissions.requestPermissions([
+      this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE,
+       this.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE,
+       this.androidPermissions.PERMISSION.MANAGE_EXTERNAL_STORAGE]);
+    
   }
   
 
@@ -39,15 +52,24 @@ export class ShatPage {
           const mensajeUser = {
             usuario: userData['usuario'],
             mensaje: this.valor,
-            fecha: date.getHours() + ":" + date.getMinutes()
+            fecha: this.agregarCero(date.getHours()) + ":" + this.agregarCero(date.getMinutes()) 
           }
 
-          var id = (date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay() + " " + date.getHours() + ":" + date.getMinutes()+ ":"  + date.getSeconds()+ ":"  + date.getMilliseconds());
+          var id = (date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay() + " " + this.agregarCero(date.getHours()) + ":" + this.agregarCero(date.getMinutes())+ ":"  + this.agregarCero(date.getSeconds())+ ":"  + this.agregarCero(date.getMilliseconds()));
           setDoc(doc(this.firestore,'Mensaje', id.toString()), mensajeUser)
           this.valor = ""
         } 
       }
     })
+  }
+
+  agregarCero(numero: number) {
+    console.log(numero)
+    if (numero < 10) {
+      return "0" + numero.toString();
+    } else {
+      return numero.toString();
+    }
   }
 
   logout() {
